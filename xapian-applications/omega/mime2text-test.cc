5953e008955350293a8e3a4ecac9882297a56de6
networkimprov <github@networkimprov.net>
2011-11-12 16:48:08 -0800

    mime2text-test processes a directory tree

--- xapian-applications/omega/mime2text-test.cc
+++ xapian-applications/omega/mime2text-test.cc
@@ -18,39 +18,69 @@
  * USA
  */
 
-#include <iostream>
-
-#include "mime2text.h"
-
-using Xapian::Mime2Text;
-
 /* Building
  *
  * ar rcs libmime2text.a \
  *   htmlparse.o  md5.o      metaxmlparse.o  pkglibbindir.o  svgparse.o   utf8convert.o  xpsxmlparse.o \
  *   loadfile.o   md5wrap.o  mime2text.o     myhtmlparse.o   runfilter.o  tmpdir.o       xmlparse.o
- * g++ -o mime2text-test mime2text-test.cc libmime2text.a -lxapian
+ * g++ -o mime2text-test mime2text-test.cc diritor.cc libmime2text.a -lxapian \
+ *   -I. -I../../xapian-core -I../../xapian-core/common
  *
  * External Dependencies (filters)
  * FIXME add these
  */
 
-int main() {
-    Mime2Text converter;
-    Mime2Text::Fields aFields;
+#include <iostream>
+#include <string>
 
-    Mime2Text::Status aStat = converter.convert("./mime-test.html", NULL, &aFields);
+#include <config.h>
 
-    std::cout << aStat
-    << " author "   << aFields.get_author()
-    << " title "    << aFields.get_title()
-    << " sample "   << aFields.get_sample()
-    << " keywords " << aFields.get_keywords()
-    << " dump "     << aFields.get_body()
-    //<< " md5 "      << aFields.get_md5()
-    << " mimetype " << aFields.get_mimetype()
-    << " command "  << aFields.get_command()
-    << std::endl;
+#include "mime2text.h"
+#include "diritor.h"
 
+using Xapian::Mime2Text;
+
+static Mime2Text converter;
+
+static void read_dir(std::string& path) {
+    path += '/';
+    DirectoryIterator d(false);
+    try {
+        d.start(path);
+        while (d.next()) {
+            std::string file = path+d.leafname();
+            switch (d.get_type()) {
+            case DirectoryIterator::REGULAR_FILE: {
+                Mime2Text::Fields aFields;
+                Mime2Text::Status aStat = converter.convert(file.c_str(), NULL, &aFields);
+
+                std::cout << aStat
+                << ", author: "   << aFields.get_author()
+                << ", title: "    << aFields.get_title()
+                << ", sample: "   << aFields.get_sample()
+                << ", keywords: " << aFields.get_keywords()
+                << ", dump: "     << aFields.get_body()
+                //<< ", md5: "      << aFields.get_md5()
+                << ", mimetype: " << aFields.get_mimetype()
+                << ", command: "  << aFields.get_command()
+                << std::endl;
+            }
+            break;
+            case DirectoryIterator::DIRECTORY:
+                read_dir(file);
+            }
+        }
+    } catch (const std::string& err) {
+        std::cerr << err << std::endl;
+    }
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+      std::cerr << "specify directory to read" << std::endl;
+      return 1;
+    }
+    std::string dir(argv[1]);
+    read_dir(dir);
     return 0;
 }
